dc4bc16b62d5de321fce746d68c0e212a047d107
SamudithNanayakkara <samudith@snappymob.com>
2022-08-08 19:09:05 +0530

    Add project PC changes

--- Test Suites/KLK TS-1.ts
+++ Test Suites/KLK TS-1.ts
@@ -14,21 +14,21 @@
    <testCaseLink>
       <guid>8b54faec-2ca2-4b5e-afb7-4030a4b9cb09</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/TC_1_KLK_Tapping</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>4e5c5160-8a0d-45cd-a75a-8971d9a9b4f6</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/TC_2_KLK_PieceMeal</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>992c9bd9-1d1c-474f-91f7-48417b09f35d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/TC_3_KLK_OC Change_FAILED</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
